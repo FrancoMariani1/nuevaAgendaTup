d034f12d65a5914c1e5351cb143c84764bd88b30
Franco Mariani <franco-mariani@hotmail.com>
2022-10-17 15:41:16 -0300

    formulario agregar

--- src/app/public/pages/agregar/agregar.module.ts
+++ src/app/public/pages/agregar/agregar.module.ts
@@ -1,17 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { AgregarRoutingModule } from './agregar-routing.module';
 import { AgregarComponent } from './agregar.component';
 
 
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+
+
+// @NgModule({
+//   imports: [
+//     BrowserModule,
+//     CommonModule,
+//     FormsModule,
+//     AgregarRoutingModule
+//   ],
+//   declarations: [
+    
+//     AgregarComponent,
+    
+    
+//   ]
+//   // providers: [],
+//   // bootstrap: [ AgregarComponent ]
+// })
+// export class AgregarModule { }
+
 @NgModule({
   declarations: [
+    
     AgregarComponent
   ],
   imports: [
     CommonModule,
-    AgregarRoutingModule
+    AgregarRoutingModule,
+    FormsModule
   ]
 })
 export class AgregarModule { }
